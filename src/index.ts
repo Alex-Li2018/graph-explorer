eee3cf302ff58b9891972d83bca6a7d40699396c
Alex-Li2018 <392064881@qq.com>
2022-11-28 23:29:21 +0800

    feat: export object

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@ import { mapNodes, mapRelationships } from './utils/mapper';
 import { NodeModel } from './models/Node';
 import { RelationshipModel } from './models/Relationship';
 
-export default GraphVisualization;
-export { mapNodes, mapRelationships, NodeModel, RelationshipModel };
+export {
+  GraphVisualization,
+  mapNodes,
+  mapRelationships,
+  NodeModel,
+  RelationshipModel,
+};
